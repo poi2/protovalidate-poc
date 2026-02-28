5ab79f57cb6992f1e2ffcaf5093771643999160d
daisuke ito <daisuke.ito.cs@gmail.com>
2026-02-28 22:50:06 +0900

    feat: Add error details in gRPC response

--- go/server/main.go
+++ go/server/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"buf.build/go/protovalidate"
@@ -14,6 +15,7 @@ import (
 	"github.com/poi2/protovalidate-poc/go/gen/user/v1/userv1connect"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -32,13 +34,79 @@ func newUserServer() (*userServer, error) {
 	}, nil
 }
 
+// toReasonCode converts protovalidate rule_id to Google API standard UPPER_SNAKE_CASE
+// e.g., "string.min_len" -> "STRING_MIN_LEN", "password_match" -> "PASSWORD_MATCH"
+func toReasonCode(ruleID string) string {
+	if ruleID == "" {
+		return ""
+	}
+	// Replace dots with underscores and convert to uppercase
+	s := strings.ReplaceAll(ruleID, ".", "_")
+	return strings.ToUpper(s)
+}
+
+func (s *userServer) handleValidationError(err error) error {
+	// protovalidateのValidationErrorを詳細に解析
+	connectErr := connect.NewError(connect.CodeInvalidArgument, err)
+
+	// ValidationErrorから詳細を取得
+	if valErr, ok := err.(*protovalidate.ValidationError); ok {
+		// BadRequestとして詳細を構造化
+		// ref: https://github.com/googleapis/googleapis/blob/master/google/rpc/error_details.proto
+		badReq := &errdetails.BadRequest{}
+
+		for _, violation := range valErr.Violations {
+			if violation.Proto == nil {
+				continue
+			}
+
+			fieldPath := ""
+			if violation.Proto.Field != nil {
+				// Field pathからフィールド名を取得
+				for _, elem := range violation.Proto.Field.Elements {
+					if elem.FieldName != nil {
+						if fieldPath != "" {
+							fieldPath += "."
+						}
+						fieldPath += *elem.FieldName
+					}
+				}
+			}
+
+			message := ""
+			if violation.Proto.Message != nil {
+				message = *violation.Proto.Message
+			}
+
+			ruleId := ""
+			if violation.Proto.RuleId != nil {
+				ruleId = *violation.Proto.RuleId
+			}
+
+			fieldViolation := &errdetails.BadRequest_FieldViolation{
+				Field:       fieldPath,
+				Description: message,
+				Reason:      toReasonCode(ruleId), // Convert to UPPER_SNAKE_CASE
+			}
+			badReq.FieldViolations = append(badReq.FieldViolations, fieldViolation)
+		}
+
+		// エラー詳細を追加
+		if detail, err := connect.NewErrorDetail(badReq); err == nil {
+			connectErr.AddDetail(detail)
+		}
+	}
+
+	return connectErr
+}
+
 func (s *userServer) CreateUser(
 	ctx context.Context,
 	req *connect.Request[userv1.CreateUserRequest],
 ) (*connect.Response[userv1.CreateUserResponse], error) {
 	// Validate the request
 	if err := s.validator.Validate(req.Msg); err != nil {
-		return nil, connect.NewError(connect.CodeInvalidArgument, err)
+		return nil, s.handleValidationError(err)
 	}
 
 	// Create a new user (simplified - in real app, would save to DB)
